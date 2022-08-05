8d518a1df45306c20078b80ae13d5321a1f35706
Creat2die <Jevgenij.s@inbox.lt>
2022-08-05 10:14:38 +0300

    joke-on-you

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -5,24 +5,31 @@ import axios from 'axios';
 
 function App() {
 
-    const [users, setUsers] = useState([]);
+    const [jokess, setJokes] = useState([]);
 
     useEffect(()=>{
 
-        axios.get('https://jsonplaceholder.typicode.com/users')
-        .then(res => setUsers(res.data));
-
+        axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
+        .then(res => setJokes(res.data.jokes));
     }, []);
+
 
     return (
       <div className="App">
         <header className="App-header"> 
-        <h1> API </h1>
-        <ul>
+        <h1> Not funny Joke </h1>
+        <p>
             {
-                users.map(u => <li key={u.id}>{u.name} from <span style= {{color: 'crimson'}}>{u.address.city}</span></li>)
+                jokess.map(u => <p key={u.id}>{u.joke}</p>)
             }
-        </ul>
+        </p>
+        <p style={{color: 'red'}}>Dvigubas prikolas</p>
+        <p>
+
+            {
+                jokess.map(u => <p style={{color: 'green'}} key={u.id}>{u.setup} <span style={{color: 'yellow'}}>{u.delivery}</span></p>)
+            }
+        </p>
         </header>
       </div>
     );
